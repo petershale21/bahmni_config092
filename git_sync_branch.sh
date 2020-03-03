7befc46111826c73587495eab86053af62ed6937
Makhate Makhate <makhatemakhate@gmail.com>
2020-03-03 15:39:55 +0200

    Update git_sync_branch.sh
    added a variable to resolve where git is supposed to run the commands from

--- git_sync_branch.sh
+++ git_sync_branch.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+# telling git where to run the commands from because the GIT_DIR enviroment
+# variable was not set
+gitDir=/var/www/bahmni_config_release/.git
+
 # checks if something happened on the remote repo
 
-git fetch
+git --git-dir=$gitDir fetch
 
 # now see if there's anything that happened
 
-git diff HEAD remotes/origin/master
+git --git-dir=$gitDir diff HEAD remotes/origin/master
 
 # Now get ONLY files that were edited. This will
 # help in preserving bandwith
 
-get fetch origin
+git --git-dir=$gitDir fetch origin
