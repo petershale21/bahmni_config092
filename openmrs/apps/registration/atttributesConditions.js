24f51a526ff39f9f94a866baaec5455de8cd5d2b
Makhate Makhate <makhatemakhate@gmail.com>
2020-12-14 23:49:12 +0200

    adding pseudocode code for enable/disable spouse

---
+++ openmrs/apps/registration/atttributesConditions.js
@@ -0,0 +1,10 @@
+var disableSpouseForMinor = function (patient){
+    var returnValues = {
+        show:[],
+        hide:[]
+    };
+    if (patient["age"].years < 10){
+        //disable Spouse relation
+        //else enable
+    }
+}
