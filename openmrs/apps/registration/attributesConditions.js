83d6450717ec3fa48fe87b3bb4b53dc028b2f284
Makhate Makhate <makhatemakhate@gmail.com>
2020-12-15 01:26:27 +0200

    checking condictions

--- openmrs/apps/registration/attributesConditions.js
+++ openmrs/apps/registration/attributesConditions.js
@@ -6,8 +6,10 @@ Bahmni.Registration.AttributesConditions.rules = {
         };
         if (a["age"].years < 10) {
             returnValues.hide.push("extraAddressInfo") //just testing
+            console.log("checking 1")
         } else {
             returnValues.show.push("extraAddressInfo")
+            console.log("checking 2")
         }
     }
 };
