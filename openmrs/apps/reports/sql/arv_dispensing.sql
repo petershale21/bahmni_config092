1fab5fb647ad78f3eaf989159d285e8a7e2e58d2
seapehim <seapehim@gmail.com>
2021-05-10 13:50:48 +0200

    pharm_report_corrections

--- openmrs/apps/reports/sql/arv_dispensing.sql
+++ openmrs/apps/reports/sql/arv_dispensing.sql
@@ -1,42 +1,21 @@
-select sm.name as Product_Description,pp.name as Pack_Size,sum(pp.qty) as Total_Pack_dispensed 
-from stock_move sm
-LEFT OUTER JOIN sale_order_line so ON sm.product_id = so.product_id
-LEFT OUTER join product_packaging pp on so.product_packaging = pp.id  
-WHERE sm.write_date >= CAST('2020-02-01' AS DATE)
-AND sm.write_date <= CAST('2020-02-28' AS DATE)
-AND so.state = 'sale'
-AND sm.name in(
-'Abacavir (ABC) 300mg',
-'Abacavir/Lamivudine (ABC/3TC) - 600/300mg',
-'Atazanavir (ATV) _300mg',
-'Atazanavir/Ritonavir (ATV/RIV) - 300/100mg',
-'Darunavir_(DVR)_300mg',
-'Darunavir (DVR) - 600mg',
-'Dolutegravir (DTG) - 50mg',
-'Efavirenz (EFV) - 600mg',
-'Etravirine (ETV) - 100mg',
-'Lamivudine (3TC) - 150mg',
-'Lopinavir/Ritonavir (LPV/r) - 200/50mg',
-'Nevirapine (NVP) - 200mg',
-'Raltegravir (RAL) - 400mg',
-'Ritonavir (RIV) - 100mg',
-'Tenofovir (TDF) - 300mg',
-'Tenofovir/Lamivudine (TDF/3TC) - 300/300mg',
-'Tenofovir/Lamivudine/Dolutegravir (TDF/3TC/DTG) - 300/300/50mg',
-'Tenofovir/Lamivudine/Efavirenz (TDF/3TC/EFV) - 300/300/400mg',
-'Tenofovir/Lamivudine/Efavirenz (TDF/3TC/EFV) - 300/300/600mg',
-'Zidovudine (AZT) - 300mg',
-'Zidovudine/Lamivudine (AZT/3TC)- 300/150mg',
-'Zidovudine/Lamivudine/Nevirapine (AZT/3TC/NVP) - 300/150/200mg',
-'Abacavir (ABC) - 60mg',
-'Abacavir/lamivudine (ABC/3TC) - 120/60mg',
-'Darunavir (DRV) - 75mg',
-'Efavirenz (EFV) - 200mg',
-'Lopinavir/Ritonavir (LPV/r) - 80mg/20ml',
-'Lopinavir/Ritonavir (LPV/r) - 40/10mg',
-'Lopinavir/Ritonavir (LPV/r) - 100/25mg ',
-'Nevirapine (NVP) - 50mg/5ml',
-'Raltegravir (RAL) - 100mg',
-'Zidovudine/Lamivudine (AZT/3TC) - 60/30mg',
-'Zidovudine/Lamivudine/Nevirapine (AZT/3TC/NVP) - 60/30/50mg')
-group by sm.name,pp.name;
+-- drug
+SELECT sm.name  Drug,CAST(sum(sol.qty_delivered) AS INTEGER) "Units Dispensed this Month",CAST(sum(pol.qty_received) AS INTEGER) "Units Received this Month",stock_on_hand as "Stock on hand"
+FROM stock_move sm
+LEFT OUTER JOIN sale_order_line sol ON  sm.product_id = sol.product_id AND sol.state = 'sale'
+LEFT OUTER JOIN purchase_order_line pol ON sm.product_id = pol.product_id AND sol.state = 'purchase'
+LEFT OUTER JOIN
+-- stock on hand this month end
+( SELECT qty stock_on_hand,a.product_id
+    FROM stock_quant a
+    INNER JOIN
+    (SELECT product_id,CAST(MAX(write_date) AS TIMESTAMP) maxdate 
+            FROM stock_quant 
+            WHERE CAST(write_date AS TIMESTAMP) <= CAST('#endDate#' AS DATE)
+            group by product_id 
+            )latest 
+            on latest.product_id = a.product_id
+    WHERE CAST(a.write_date AS TIMESTAMP) = maxdate
+    AND a.write_date >= CAST('#startDate#' AS DATE)
+    AND a.write_date <= CAST('#endDate#' AS DATE)
+)stock ON sm.product_id = stock.product_id
+GROUP BY sm.name,stock_on_hand
