4c2d4ab9913a83f08765834315f4e5a4ca8445d8
mahaomolise92 <mahaomolise92@gmail.com>
2021-09-22 08:43:05 +0200

    fixing an error that appears when importing patients

--- openmrs/apps/registration/attributesConditions.js
+++ openmrs/apps/registration/attributesConditions.js
@@ -1,15 +1,15 @@
-Bahmni.Registration.AttributesConditions.rules = {
-    'age': function (a) {
-        var returnValues = {
-            show:[],
-            hide:[]
-        };
-        if (a["age"].years < 10) {
-            returnValues.hide.push("extraAddressInfo") //just testing
-            console.log("checking 1");
-        } else {
-            returnValues.show.push("extraAddressInfo")
-            console.log("checking 2");
-        }
-    }
-};
+// Bahmni.Registration.AttributesConditions.rules = {
+//     'age': function (a) {
+//         var returnValues = {
+//             show: [],
+//             hide: []
+//         };
+//         if (a["age"].years < 10) {
+//             returnValues.hide.push("extraAddressInfo") //just testing
+//             console.log("checking 1");
+//         } else {
+//             returnValues.show.push("extraAddressInfo")
+//             console.log("checking 2");
+//         }
+//     }
+// };
