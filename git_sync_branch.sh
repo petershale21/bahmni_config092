ff96fd0fb495276b9ffafdf6dd455cea01ec562d
khatman <makhatemakhate@gmail.com>
2021-11-02 10:45:00 +0200

    fixing automated deployment scripts

--- git_sync_branch.sh
+++ git_sync_branch.sh
@@ -1,18 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# telling git where to run the commands from because the GIT_DIR enviroment
-# variable was not set
-gitDir=/var/www/bahmni_config_release/.git
-
-# checks if something happened on the remote repo
-
-git --git-dir=$gitDir fetch
-
-# now see if there's anything that happened
-
-git --git-dir=$gitDir diff HEAD remotes/origin/master
-
-# Now get ONLY files that were edited. This will
-# help in preserving bandwith
-
-git --git-dir=$gitDir fetch origin
+git fetch --all
+git pull origin master
