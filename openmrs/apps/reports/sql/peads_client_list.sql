601255e8aa8df5f0630a60909a3bba5b5ea804da
seapehim <seapehim@gmail.com>
2022-02-15 12:18:55 +0200

    Client List Modified, included Missed,ART Start, ART Initiated

--- openmrs/apps/reports/sql/peads_client_list.sql
+++ openmrs/apps/reports/sql/peads_client_list.sql
@@ -1,4 +1,14 @@
-(SELECT Id,patientIdentifier AS "Patient_Identifier",ART_Number, patientName AS "Patient_Name", Age,DOB, Gender, 'Initiated' AS 'Program_Status'
+SELECT  distinct Patient_Identifier,
+			     ART_Number,
+				 Patient_Name,
+				 DOB,
+				 Age,
+				 Gender, 
+				 Program_Status, 
+				 ART_Initiation_Date,
+				 Date_Diagnosed
+FROM
+((SELECT Id,patientIdentifier AS "Patient_Identifier",ART_Number, patientName AS "Patient_Name",DOB, Age, Gender, 'Initiated' AS 'Program_Status'
 	-- SELECT  patientIdentifier , patientName, Age, Gender, age_group, 'Initiated' AS 'Program_Status', sort_order
 	FROM
                 (select distinct patient.patient_id AS Id,
@@ -42,7 +52,7 @@ ORDER BY Newly_Initiated_ART_Clients.patientName)
 
 UNION
 
-(SELECT Id,patientIdentifier AS "Patient_Identifier",ART_Number, patientName AS "Patient_Name", Age,DOB, Gender, 'Seen' AS 'Program_Status'
+(SELECT Id,patientIdentifier AS "Patient_Identifier",ART_Number, patientName AS "Patient_Name",DOB, Age, Gender, 'Seen' AS 'Program_Status'
 	-- SELECT patientIdentifier , patientName , Age, Gender, age_group, 'Seen' AS 'Program_Status', sort_order
 FROM (
 
@@ -59,16 +69,16 @@ select distinct patient.patient_id AS Id,
 								-- CLIENTS SEEN FOR ART
                                  INNER JOIN patient ON o.person_id = patient.patient_id
                                  AND (o.concept_id = 3843 AND o.value_coded = 3841 OR o.value_coded = 3842)
-								 AND MONTH(o.obs_datetime) >= MONTH(CAST('2021-01-01' AS DATE)) 
-								 AND MONTH(o.obs_datetime) <= MONTH(CAST('2021-03-31' AS DATE))
-								 AND YEAR(o.obs_datetime) = YEAR(CAST('2021-03-31' AS DATE))
+								 AND MONTH(o.obs_datetime) >= MONTH(CAST('#startDate#' AS DATE)) 
+								 AND MONTH(o.obs_datetime) <= MONTH(CAST('#endDate#' AS DATE))
+								 AND YEAR(o.obs_datetime) = YEAR(CAST('#endDate#' AS DATE))
                                  AND patient.voided = 0 AND o.voided = 0
                                  INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
                                  INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
                                  INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
 								 LEFT OUTER JOIN patient_identifier p ON p.patient_id = person.person_id AND p.identifier_type = 5
 								 INNER JOIN reporting_age_group AS observed_age_group ON
-									  CAST('2021-03-31' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+									  CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
 									  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
            WHERE observed_age_group.report_group_name = 'Modified_Ages'
 
@@ -164,7 +174,7 @@ ORDER BY Clients_Seen.patientName)
 
 UNION
 
-(SELECT Id,patientIdentifier AS "Patient_Identifier",ART_Number, patientName AS "Patient_Name", Age,DOB, Gender, 'Seen_Previous' AS 'Program_Status'
+(SELECT Id,patientIdentifier AS "Patient_Identifier",ART_Number, patientName AS "Patient_Name", DOB,Age, Gender, 'Seen_Previous' AS 'Program_Status'
 FROM (
 (select distinct patient.patient_id AS Id,
 									   patient_identifier.identifier AS patientIdentifier,
@@ -277,3 +287,114 @@ FROM (
 ) AS ARTCurrent_PrevMonths
  WHERE Age <=19
 ORDER BY ARTCurrent_PrevMonths.Age)
+
+UNION
+
+-- INCLUDE MISSED APPOINTMENTS WITHIN 28 DAYS ACCORDING TO THE NEW PEPFAR GUIDELINE
+(SELECT Id,patientIdentifier AS "Patient_Identifier",ART_Number, patientName AS "Patient_Name", DOB, Age, Gender,'MissedWithin28Days' AS 'Program_Status'
+FROM
+                 (select distinct patient.patient_id AS Id,
+									   patient_identifier.identifier AS patientIdentifier,
+									   p.identifier as ART_Number,
+									   concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
+									   floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/365) AS Age,
+									   person.birthdate as DOB,
+									   person.gender AS Gender,
+									   observed_age_group.name AS age_group,
+									   observed_age_group.sort_order AS sort_order
+                from obs o
+						-- CLIENTS WHO MISSED APPOINTMENTS < 28 DAYS
+						 INNER JOIN patient ON o.person_id = patient.patient_id
+						 AND o.person_id in (
+						 -- begin
+						select active_clients.person_id
+								from
+								(select B.person_id, B.obs_group_id, B.value_datetime AS latest_follow_up
+									from obs B
+									inner join 
+									(select person_id, max(obs_datetime), SUBSTRING(MAX(CONCAT(obs_datetime, obs_id)), 20) AS observation_id
+									from obs where concept_id = 3753
+									and obs_datetime <= cast('#endDate#' as date)
+									and voided = 0
+									group by person_id) as A
+									on A.observation_id = B.obs_group_id
+									where concept_id = 3752
+									and A.observation_id = B.obs_group_id
+                                    and voided = 0	
+									group by B.person_id
+								) as active_clients
+								where active_clients.latest_follow_up < cast('#endDate#' as date)
+								and DATEDIFF(CAST('#endDate#' AS DATE),latest_follow_up) <= 28
+				
+				
+		and active_clients.person_id not in (
+							select distinct os.person_id
+							from obs os
+							where (os.concept_id = 3843 AND os.value_coded = 3841 OR os.value_coded = 3842)
+							AND MONTH(os.obs_datetime) >= MONTH(CAST('#startDate#' AS DATE)) 
+							AND MONTH(os.obs_datetime) <= MONTH(CAST('#endDate#' AS DATE))
+							AND YEAR(os.obs_datetime) = YEAR(CAST('#endDate#' AS DATE))
+
+
+							)
+						
+		and active_clients.person_id not in (
+							select distinct os.person_id
+							from obs os
+							where concept_id = 2249
+							AND MONTH(os.obs_datetime) >= MONTH(CAST('#startDate#' AS DATE)) 
+							AND MONTH(os.obs_datetime) <= MONTH(CAST('#endDate#' AS DATE))
+							AND YEAR(os.obs_datetime) = YEAR(CAST('#endDate#' AS DATE))
+							)
+
+		and active_clients.person_id not in (
+							
+									-- TOUTS
+									select distinct(person_id)
+									from
+									(
+										select os.person_id, CAST(max(os.obs_datetime) AS DATE) as latest_transferout
+										from obs os
+										where os.concept_id=2398
+										group by os.person_id
+										having latest_transferout <= CAST('#endDate#' AS DATE)
+									) as TOUTS
+										
+										)
+			
+
+		and active_clients.person_id not in (
+									select person_id 
+									from person 
+									where death_date <= cast('#endDate#' as date)
+									and dead = 1
+						 )
+						 )
+						 -- end
+						 
+						 INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+						 INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+						 INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
+						 LEFT OUTER JOIN patient_identifier p ON p.patient_id = person.person_id AND p.identifier_type = 5
+						 INNER JOIN reporting_age_group AS observed_age_group ON
+						  CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+						  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+                   WHERE observed_age_group.report_group_name = 'Modified_Ages') AS TwentyEightDayDefaulters
+				   WHERE Age <=19
+				   order by TwentyEightDayDefaulters.patientName)
+)active_clients
+
+-- ART START	
+	left outer join
+	(
+	select person_id,CAST(value_datetime AS DATE) as ART_Initiation_Date
+	from obs where concept_id = 2249 and voided = 0
+	)intake_date
+	on active_clients.Id = intake_date.person_id
+-- Diagnosis Date	
+	left outer join
+	(
+	select person_id,CAST(value_datetime AS DATE) as Date_Diagnosed
+	from obs where concept_id = 2222 and voided = 0
+	)diagnosis_date
+	on active_clients.Id = diagnosis_date.person_id
