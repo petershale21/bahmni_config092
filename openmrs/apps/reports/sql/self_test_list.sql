eaf331cdeea3371d4d7b3c80059366124249bf2d
seapehim <seapehim@gmail.com>
2022-02-16 13:06:23 +0200

    Self Test Report Modified,adding HTS completeness reports

--- openmrs/apps/reports/sql/self_test_list.sql
+++ openmrs/apps/reports/sql/self_test_list.sql
@@ -1,10 +1,10 @@
 
-Select distinct Patient_Identifier, Patient_Name,Age, Gender, age_group,HIV_Testing_Initiation, HIV_Status,Distribution_Date
+Select Patient_Identifier, Patient_Name,Age, Gender, age_group,HIV_Testing_Initiation, HIV_Status, Distribution_Date, Kits_Distributed, Kits_Returned, Positive_Kits
 from(
 SELECT Id,Patient_Identifier, Patient_Name, Age, Gender, age_group, HIV_Testing_Initiation  , HIV_Status
 FROM (
 (SELECT Id,patientIdentifier AS "Patient_Identifier", patientName AS "Patient_Name", Age, Gender, age_group, 'Self-test' AS 'HIV_Testing_Initiation'
-                          ,HIV_Status, sort_order
+                          , HIV_Status, sort_order
 		FROM
 						(select distinct patient.patient_id AS Id,
 											   patient_identifier.identifier AS patientIdentifier,
@@ -33,8 +33,8 @@ FROM (
 								 )
 								 
 								 INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
-								 INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
-								 INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
+								 INNER JOIN person_name ON person.person_id = person_name.person_id
+								 INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3
 								 INNER JOIN reporting_age_group AS observed_age_group ON
 								  CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
 								  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
@@ -73,8 +73,8 @@ UNION
 								 )
 								 
 								 INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
-								 INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
-								 INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
+								 INNER JOIN person_name ON person.person_id = person_name.person_id
+								 INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3
 								 INNER JOIN reporting_age_group AS observed_age_group ON
 								  CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
 								  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
@@ -103,18 +103,18 @@ UNION
 								 AND YEAR(o.obs_datetime) = YEAR(CAST('#endDate#' AS DATE))
 								 
 								 -- HAS HIV UNKNOWN RESULTS 
-								 AND o.person_id in (
+								    AND o.person_id in (
 									select distinct os.person_id
 									from obs os
 									where os.concept_id = 4844 and os.value_coded = 2148
 									AND MONTH(o.obs_datetime) = MONTH(CAST('#endDate#' AS DATE)) 
-								 AND YEAR(o.obs_datetime) = YEAR(CAST('#endDate#' AS DATE))
+								    AND YEAR(o.obs_datetime) = YEAR(CAST('#endDate#' AS DATE))
 									AND patient.voided = 0 AND o.voided = 0
 								 )
 								 
 								 INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
-								 INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
-								 INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
+								 INNER JOIN person_name ON person.person_id = person_name.person_id
+								 INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3
 								 INNER JOIN reporting_age_group AS observed_age_group ON
 								  CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
 								  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
@@ -123,8 +123,8 @@ UNION
 		ORDER BY HTSClients_HIV_Status.HIV_Status, HTSClients_HIV_Status.Age)
 		
 ) AS HTS_Status_Detailed
-ORDER BY HTS_Status_Detailed.HIV_Status desc
-			, HTS_Status_Detailed.sort_order ) AS SelfTest
+ORDER BY HTS_Status_Detailed.HIV_Testing_Initiation
+			, HTS_Status_Detailed.sort_order) AS SelfTest
 
 -- DISTRIBUTION DATE
 left outer join
@@ -133,7 +133,64 @@ left outer join
 	from obs o
 	INNER JOIN patient ON o.person_id = patient.patient_id
 	 where o.concept_id = 4824
-	 AND MONTH(o.value_datetime) = MONTH(CAST('#endDate#' AS DATE)) 
-	 AND YEAR(o.value_datetime) = YEAR(CAST('#endDate#' AS DATE))
 	) as distibutedDate
 	on SelfTest.Id = distibutedDate.patient_id
+
+-- Number of Distributed Kits	
+Left outer join
+(
+	select o.person_id as Id, (count(value_text) + 1) as Kits_Distributed
+             from obs o
+                INNER JOIN patient ON o.person_id = patient.patient_id 
+                INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+                INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+                INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type IN (3,5) AND patient_identifier.preferred=1
+                
+                AND o.voided=0
+                AND MONTH(o.obs_datetime) = MONTH(CAST('#endDate#' AS DATE)) 
+               AND YEAR(o.obs_datetime) = YEAR(CAST('#endDate#' AS DATE))
+								                
+                where concept_id in (4968)
+				Group by o.person_id
+ ) as num_distributed
+ on SelfTest.Id = num_distributed.Id
+
+ -- Number of kits returned
+
+Left outer join
+(
+	select o.person_id as Id, count(value_coded) as Kits_Returned
+        from obs o
+    INNER JOIN patient ON o.person_id = patient.patient_id 
+    INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+    INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+    INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type IN (3,5) AND patient_identifier.preferred=1
+                
+    AND o.voided=0
+    AND MONTH(o.obs_datetime) = MONTH(CAST('#endDate#' AS DATE)) 
+    AND YEAR(o.obs_datetime) = YEAR(CAST('#endDate#' AS DATE))
+								                
+    where concept_id in (4844) and value_coded not in (2148)
+	Group by o.person_id
+) as num_returned
+on SelfTest.Id = num_returned.Id
+
+-- Number of confirmed positives
+
+Left outer join
+(
+	select o.person_id as Id, count(value_coded) as Positive_Kits
+        from obs o
+    INNER JOIN patient ON o.person_id = patient.patient_id 
+    INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+    INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+    INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type IN (3,5) AND patient_identifier.preferred=1
+                
+    AND o.voided=0
+    AND MONTH(o.obs_datetime) = MONTH(CAST('#endDate#' AS DATE)) 
+    AND YEAR(o.obs_datetime) = YEAR(CAST('#endDate#' AS DATE))
+								                
+    where concept_id in (4844) and value_coded in (1738)
+	Group by o.person_id
+) as positive_kits
+on SelfTest.Id = positive_kits.Id
