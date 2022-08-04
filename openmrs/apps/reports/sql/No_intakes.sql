6c7dd2e6fc48c9c974cf0306dd7d0906cebd374a
Masupha <46922883+Masupha@users.noreply.github.com>
2022-08-04 11:24:29 +0200

    revised_intake_reports

--- openmrs/apps/reports/sql/No_intakes.sql
+++ openmrs/apps/reports/sql/No_intakes.sql
@@ -1,21 +1,34 @@
-SELECT distinct patientIdentifier AS "Patient Identifier", patientName AS "Patient Name"
+SELECT distinct patientIdentifier AS "Patient Identifier", ART_Number AS "ART Number", patientName AS "Patient Name", age_group AS "Age Group", Age, Gender, location_name AS "Location"
 
 FROM
         (
 			SELECT distinct 
-							p.identifier AS patientIdentifier,
-							concat(pn.given_name, ' ', pn.family_name) AS patientName
+							patient_identifier.identifier AS patientIdentifier,
+							p.identifier AS ART_Number,
+							concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
+							observed_age_group.name AS age_group,
+							floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/365) AS Age,
+							person.gender AS Gender,
+							l.name as location_name
 							
 			FROM obs o
-									INNER JOIN patient_identifier p ON o.person_id = p.patient_id 
-									INNER JOIN person_name pn ON p.patient_id = pn.person_id
-									AND o.person_id not in
-									(				
-										select person_id from obs where concept_id = 2249
-									)								
+								INNER JOIN patient ON o.person_id = patient.patient_id
+								INNER JOIN patient_identifier p ON o.person_id = p.patient_id 
+								INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+								INNER JOIN person_name ON person.person_id = person_name.person_id
+								INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3
+								JOIN location l on o.location_id = l.location_id and l.retired=0
+								INNER JOIN reporting_age_group AS observed_age_group ON
+								CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+								AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+						   		WHERE observed_age_group.report_group_name = 'Modified_Ages'
+								AND o.person_id not in
+								(				
+									select person_id from obs where concept_id = 2249
+								)								
 									
-									WHERE p.identifier_type = 5 
-									AND o.voided = 0
+								AND p.identifier_type = 5 
+								AND o.voided = 0
 			
 		) AS Patient_MissedAppointments
 
