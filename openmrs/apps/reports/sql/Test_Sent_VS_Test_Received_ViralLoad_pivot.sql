cc42ab96a47f58516661c7f6902e31e89ff9e5b6
seapehim <seapehim@gmail.com>
2024-06-04 15:31:23 +0200

    Corrected misalignment of Viral Load Tests done reports - list and pivot

--- openmrs/apps/reports/sql/Test_Sent_VS_Test_Received_ViralLoad_pivot.sql
+++ openmrs/apps/reports/sql/Test_Sent_VS_Test_Received_ViralLoad_pivot.sql
@@ -21,19 +21,20 @@ FROM
 
 	(
 	
-            Select Id, patientIdentifier as Patient_Identifier, ART_Number as "ART Number", patientName as Patient_Name, age_group, Age, Gender, 
-        date_collected as "Date Specimen Collected",Test,Results, received, sort_order
+            Select Id, patientIdentifier as Patient_Identifier,  patientName as Patient_Name, age_group, Age, Gender, 
+        date_collected as "Date Specimen Collected",Test,Results, sort_order
 From
 (select distinct patient.patient_id AS Id,
                 patient_identifier.identifier AS patientIdentifier,
-                p.identifier as ART_Number,
+                -- p.identifier as ART_Number,
                 concat(person_name.given_name, " ", person_name.family_name) AS patientName,
                 floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/365) AS Age,
                 person.gender AS Gender,
                 cast(o.date_created as date) as date_collected,
                 observed_age_group.name AS age_group,
                 "Done" AS Test,
-                observed_age_group.sort_order AS sort_order
+                observed_age_group.sort_order AS sort_order,
+		        order_id
 
             from orders o
             -- Viral Load Tests Sent
@@ -45,37 +46,50 @@ From
                     CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
                     AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
                     AND o.concept_id = 5484
-                    LEFT OUTER JOIN patient_identifier p ON p.patient_id = person.person_id AND p.identifier_type = 5
-                WHERE observed_age_group.report_group_name = 'Modified_Ages'
-                AND CAST(o.date_created AS DATE) >= CAST('#startDate#' AS DATE)
-                AND CAST(o.date_created AS DATE) <= CAST('#endDate#' AS DATE)) as test
+                    -- LEFT OUTER JOIN patient_identifier p ON p.patient_id = person.person_id AND p.identifier_type = 5
+                    WHERE observed_age_group.report_group_name = 'Modified_Ages'
+                    AND CAST(o.date_created AS DATE) >= CAST('#startDate#' AS DATE)
+                    AND CAST(o.date_created AS DATE) <= CAST('#endDate#' AS DATE)) as test
+
 Left Outer Join
-(
-    Select pId, Results, received
+
+(Select person_id, Lab_Order_Number, lab_order.order_id as orders_id , VL_result.order_id, VL_result.Results, result_date 
+FROM
+(select person_id, cast(obs_datetime as date)as max_observation, SUBSTRING(CONCAT(obs_datetime, obs_id), 20) AS observation_id, order_id, value_text  as Lab_Order_Number
+        from obs where concept_id = 5498 -- Lab order number
+	    and cast(obs_datetime as date) >= cast('#startDate#' as date)
+        and cast(obs_datetime as date) <= cast('#endDate#' as date)
+        and voided = 0
+        -- group by person_id
+		)lab_order
+
+left outer join
+
+(Select pId, result.order_id, Results, cast(obs_datetime as date) as result_date
     From
       (
-        select oss.person_id as pId, concat(oss.value_numeric, " ", "copies/ml")  as Results, "Received" as received
+        select oss.person_id as pId, concat(oss.value_numeric, " ", "copies/ml")  as Results, order_id, oss.obs_datetime
             from obs oss
             where oss.concept_id = 5485
             and oss.voided=0
-            and cast(oss.obs_datetime as date) <= cast('#endDate#' as date)
+            and cast(oss.obs_datetime as date) >= cast('#startDate#' as date)
             -- group by oss.person_id
 
     UNION
 
-    select oss.person_id as pId, "LDL"  as Results, "Received" as received
+    select oss.person_id as pId, "LDL"  as Results, order_id, oss.obs_datetime
             from obs oss
             where oss.concept_id = 5489
             and oss.voided=0
-            and cast(oss.obs_datetime as date)  <= cast('#endDate#' as date)
+            and cast(oss.obs_datetime as date)  >= cast('#startDate#' as date)
             -- group by oss.person_id
 
-      ) As received_results
-    
+      )result
+      )VL_result
+	  on lab_order.order_id = VL_result.order_id
 
-) as received_results
-on test.Id = received_results.pId
-
+)lab_orders	
+on test.order_id = lab_orders.orders_id
 
 
 	) AS VL_DETAILS
@@ -102,25 +116,25 @@ FROM
 					, Total_VL.Age
 					, Total_VL.Gender
 					, Total_VL.Test
-                    , Total_VL.received
 				
 		FROM
 
 		(
 
-                Select Id, patientIdentifier as Patient_Identifier, ART_Number as "ART Number", patientName as Patient_Name,age_group, Age, Gender, 
-        date_collected as "Date Specimen Collected",Test,Results, received, sort_order
+             Select Id, patientIdentifier as Patient_Identifier,  patientName as Patient_Name, age_group, Age, Gender, 
+        date_collected as "Date Specimen Collected",Test,Results sort_order
 From
 (select distinct patient.patient_id AS Id,
                 patient_identifier.identifier AS patientIdentifier,
-                p.identifier as ART_Number,
+                -- p.identifier as ART_Number,
                 concat(person_name.given_name, " ", person_name.family_name) AS patientName,
                 floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/365) AS Age,
                 person.gender AS Gender,
                 cast(o.date_created as date) as date_collected,
                 observed_age_group.name AS age_group,
                 "Done" AS Test,
-                observed_age_group.sort_order AS sort_order
+                observed_age_group.sort_order AS sort_order,
+		order_id
 
             from orders o
             -- Viral Load Tests Sent
@@ -132,42 +146,53 @@ From
                     CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
                     AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
                     AND o.concept_id = 5484
-                    LEFT OUTER JOIN patient_identifier p ON p.patient_id = person.person_id AND p.identifier_type = 5
-                WHERE observed_age_group.report_group_name = 'Modified_Ages'
-                AND CAST(o.date_created AS DATE) >= CAST('#startDate#' AS DATE)
-                AND CAST(o.date_created AS DATE) <= CAST('#endDate#' AS DATE)) as test
+                    -- LEFT OUTER JOIN patient_identifier p ON p.patient_id = person.person_id AND p.identifier_type = 5
+                    WHERE observed_age_group.report_group_name = 'Modified_Ages'
+                    AND CAST(o.date_created AS DATE) >= CAST('#startDate#' AS DATE)
+                    AND CAST(o.date_created AS DATE) <= CAST('#endDate#' AS DATE)) as test
+
 Left Outer Join
-(
-    Select pId, Results, received
+
+(Select person_id, Lab_Order_Number, lab_order.order_id as orders_id , VL_result.order_id, VL_result.Results, result_date 
+FROM
+(select person_id, cast(obs_datetime as date)as max_observation, SUBSTRING(CONCAT(obs_datetime, obs_id), 20) AS observation_id, order_id, value_text  as Lab_Order_Number
+        from obs where concept_id = 5498 -- Lab order number
+	and cast(obs_datetime as date) >= cast('#startDate#' as date)
+        and cast(obs_datetime as date) <= cast('#endDate#' as date)
+        and voided = 0
+        -- group by person_id
+		)lab_order
+
+left outer join
+
+(Select pId, result.order_id, Results, cast(obs_datetime as date) as result_date
     From
       (
-        select oss.person_id as pId, concat(oss.value_numeric, " ", "copies/ml")  as Results, "Received" as received
+        select oss.person_id as pId, concat(oss.value_numeric, " ", "copies/ml")  as Results, order_id, oss.obs_datetime
             from obs oss
             where oss.concept_id = 5485
             and oss.voided=0
-            and cast(oss.obs_datetime as date)  >= cast('#startDate#' as date)
-            and cast(oss.obs_datetime as date) <= cast('#endDate#' as date)
+            and cast(oss.obs_datetime as date) >= cast('#startDate#' as date)
             -- group by oss.person_id
 
     UNION
 
-    select oss.person_id as pId, "LDL"  as Results, "Received" as received
+    select oss.person_id as pId, "LDL"  as Results, order_id, oss.obs_datetime
             from obs oss
             where oss.concept_id = 5489
             and oss.voided=0
             and cast(oss.obs_datetime as date)  >= cast('#startDate#' as date)
-            and cast(oss.obs_datetime as date)  <= cast('#endDate#' as date)
-           -- group by oss.person_id
+            -- group by oss.person_id
 
-      ) As received_results
-    
+      )result
+      )VL_result
+	  on lab_order.order_id = VL_result.order_id
 
-) as received_results
-on test.Id = received_results.pId
+)lab_orders	
+on test.order_id = lab_orders.orders_id
 
 
-
-		) AS Total_VL
+	) AS Total_VL
   ) AS Totals
  )
 ) AS Total_Aggregated_VL
