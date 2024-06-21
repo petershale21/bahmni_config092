59c50ca90ab7da14d9a0b37953600ea5a1deae31
KekeletsoNtho <kekentho@gmail.com>
2024-06-21 16:18:20 +0200

    PMTCT_ART, PMTCT_EID_list, PMTCT_FO_list,Prep_CT

--- openmrs/apps/reports/sql/egpaf/PMTCT_EID_list.sql
+++ openmrs/apps/reports/sql/egpaf/PMTCT_EID_list.sql
@@ -1,44 +1,116 @@
-SELECT Patient_Identifier, Patient_Name, Age, Gender, Age_at_Test, Test_Sequence, Date_Sample_Taken, Test_Result
+SELECT distinct Patient_Identifier, Patient_Name, Age, Gender, Age_at_Test, Test_Sequence, Date_Sample_Taken, Test_Result
 FROM 
 
-		(SELECT Id, patientIdentifier AS "Patient_Identifier", patientName AS "Patient_Name", Age_, Age, Gender, age_group, sort_order, group_id
+		(SELECT distinct Id, patientIdentifier AS "Patient_Identifier", patientName AS "Patient_Name", Age_, Age, Gender, age_group, sort_order, group_id
 		FROM
 						(select distinct patient.patient_id AS Id,
-							patient_identifier.identifier AS patientIdentifier,
-							concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
-							floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/30) AS Age_,
-							concat(floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/30), ' ', 'months') as Age,
-							person.gender AS Gender,
-							o.obs_group_id as group_id,
-							observed_age_group.name AS age_group,
-							observed_age_group.sort_order AS sort_order
+										patient_identifier.identifier AS patientIdentifier,
+										concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
+										floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/30) AS Age_,
+										concat(floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/30), ' ', 'months') as Age,
+										person.gender AS Gender,
+										o.obs_group_id as group_id,
+										observed_age_group.name AS age_group,
+										observed_age_group.sort_order AS sort_order
 
 						from obs o
-								 INNER JOIN patient ON o.person_id = patient.patient_id 
-								 AND patient.voided = 0 AND o.voided = 0
-								 AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
-								 
-                                 AND o.person_id in (
-									select distinct os.person_id 
-									from obs os
-									where os.concept_id in (4569, 4588, 5095) -- First NAT test, Repeat NAT and Second NAT test at 9months
-									AND cast(os.obs_datetime as date) >= CAST('#startDate#' AS DATE) 
-									AND cast(os.obs_datetime as date) <= CAST('#endDate#' AS DATE)
-									AND os.voided = 0
-								 )
-								 
-								 INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
-								 INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
-								 INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
-								 INNER JOIN reporting_age_group AS observed_age_group ON
-								  CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
-								  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
-						   WHERE observed_age_group.report_group_name = 'Modified_Ages'
-								 -- First NAT test, Repeat NAT and Second NAT test at 9months
-								 AND o.obs_group_id in (
-									select og.obs_id from obs og where og.concept_id in (4569, 4588, 5095)
-								 )) AS HEIClients_Status
-                                 having Age_ <= 12   
+						inner join patient ON o.person_id = patient.patient_id 
+						inner join (
+									select distinct oss.person_id, MAX(oss.obs_datetime) as max_observation,
+										SUBSTRING(MAX(CONCAT(oss.obs_datetime, oss.value_coded)), 20) as examination_timing, max(oss.encounter_id) as encounterId
+									from obs oss
+									where oss.concept_id = 4569
+									and cast(oss.obs_datetime as date) <= cast('#endDate#' as date)
+										group by oss.person_id
+									)latest
+									on latest.person_id = o.person_id
+									INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+									INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+									INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
+									INNER JOIN reporting_age_group AS observed_age_group ON
+										CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+									AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+									WHERE observed_age_group.report_group_name = 'Modified_Ages'
+									AND o.concept_id = 4575
+									AND o.voided=0
+									AND cast(o.obs_datetime as date) = cast(max_observation as date)
+									AND CAST(o.value_datetime AS DATE) >= CAST('#startDate#' AS DATE)
+									AND CAST(o.value_datetime AS DATE) <= CAST('#endDate#' AS DATE)
+
+						UNION
+
+						select distinct patient.patient_id AS Id,
+										patient_identifier.identifier AS patientIdentifier,
+										concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
+										floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/30) AS Age_,
+										concat(floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/30), ' ', 'months') as Age,
+										person.gender AS Gender,
+										o.obs_group_id as group_id,
+										observed_age_group.name AS age_group,
+										observed_age_group.sort_order AS sort_order
+
+						from obs o
+						inner join patient ON o.person_id = patient.patient_id 
+						inner join (
+									select distinct  oss.person_id, MAX(oss.obs_datetime) as max_observation,
+										SUBSTRING(MAX(CONCAT(oss.obs_datetime, oss.value_coded)), 20) as examination_timing, max(oss.encounter_id) as encounterId
+									from obs oss
+									where oss.concept_id = 4588
+									and cast(oss.obs_datetime as date) <= cast('#endDate#' as date)
+										group by oss.person_id
+									)latest
+									on latest.person_id = o.person_id
+									INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+									INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+									INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
+									INNER JOIN reporting_age_group AS observed_age_group ON
+										CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+									AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+									WHERE observed_age_group.report_group_name = 'Modified_Ages'
+									AND o.concept_id = 4575
+									AND o.voided=0
+									AND cast(o.obs_datetime as date) = cast(max_observation as date)
+									AND CAST(o.value_datetime AS DATE) >= CAST('#startDate#' AS DATE)
+									AND CAST(o.value_datetime AS DATE) <= CAST('#endDate#' AS DATE)
+
+
+						UNION
+
+						select distinct patient.patient_id AS Id,
+										patient_identifier.identifier AS patientIdentifier,
+										concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
+										floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/30) AS Age_,
+										concat(floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/30), ' ', 'months') as Age,
+										person.gender AS Gender,
+										o.obs_group_id as group_id,
+										observed_age_group.name AS age_group,
+										observed_age_group.sort_order AS sort_order
+
+						from obs o
+						inner join patient ON o.person_id = patient.patient_id 
+						inner join (
+									select distinct oss.person_id, MAX(oss.obs_datetime) as max_observation,
+										SUBSTRING(MAX(CONCAT(oss.obs_datetime, oss.value_coded)), 20) as examination_timing, max(oss.encounter_id) as encounterId
+									from obs oss
+									where oss.concept_id = 5095
+									and cast(oss.obs_datetime as date) <= cast('#endDate#' as date)
+										group by oss.person_id
+									)latest
+									on latest.person_id = o.person_id
+									INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+									INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+									INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
+									INNER JOIN reporting_age_group AS observed_age_group ON
+										CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+									AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+									WHERE observed_age_group.report_group_name = 'Modified_Ages'
+									AND o.concept_id = 4575
+									AND o.voided=0
+									AND cast(o.obs_datetime as date) = cast(max_observation as date)
+									AND CAST(o.value_datetime AS DATE) >= CAST('#startDate#' AS DATE)
+									AND CAST(o.value_datetime AS DATE) <= CAST('#endDate#' AS DATE)
+								 ) AS HEIClients_Status
+                            having Age_ <= 12   
 
 
 
